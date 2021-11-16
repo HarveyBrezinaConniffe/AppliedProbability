09209a5b7973ea0523b273e830667720a91a3a5b
Harvey Brezina Conniffe <harvey@Harveys-MacBook-Pro.local>
2021-11-16 15:13:00 +0000

    Started work

--- game1.py
+++ game1.py
@@ -1,1 +1,21 @@
-print("Hello!")
+import random as rand
+
+def simulateOneGame():
+	slot = rand.randint(0, 36)
+	if slot != 0 and slot%2 == 0:
+		return 1
+	return -1
+
+# Simulate 10,000 games
+winnings = []
+for i in range(10000):
+	winnings.append(simulateOneGame())
+
+# Expected winnings
+expectedWinnings = 0
+for winning in winnings:
+	expectedWinnings += winning
+expectedWinnings /= len(winnings)
+
+print("Game 1:")
+print("Expected winnings: {}".format(expectedWinnings))
