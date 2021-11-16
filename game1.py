9a440b90315f76f905a0e527bcd05fca9f452242
Harvey Brezina Conniffe <harvey@Harveys-MacBook-Pro.local>
2021-11-16 16:13:44 +0000

    Added helper library.

--- game1.py
+++ game1.py
@@ -1,41 +1,10 @@
 import random as rand
+import helpers
 
 def simulateOneGame():
 	slot = rand.randint(0, 36)
 	if slot != 0 and slot%2 == 0:
-		return 1
-	return -1
+		return 1, 1
+	return -1, 1
 
-# Simulate 10,000 games
-winnings = []
-for i in range(10000):
-	winnings.append(simulateOneGame())
-
-# Expected winnings
-expectedWinnings = 0
-for winning in winnings:
-	expectedWinnings += winning
-expectedWinnings /= len(winnings)
-
-# Proportion of wins
-wins = 0
-for winning in winnings:
-	if winning > 0:
-		wins += 1
-winProportion = wins/len(winnings)
-
-# Expected playing time
-playingTime = 1
-
-# Maximum loss
-maxLoss = abs(min(winnings))
-
-# Maximum win
-maxWin = max(winnings)
-
-print("Game 1:")
-print("Expected winnings: {}".format(expectedWinnings))
-print("Proportion of games won: {}".format(winProportion))
-print("Expected playing time per game: {}".format(playingTime))
-print("Maximum loss: ${}".format(maxLoss))
-print("Maximim win: ${}".format(maxWin))
+helpers.simulateGame(simulateOneGame, 10000)
---
+++ helpers.py
@@ -0,0 +1,42 @@
+def simulateGame(game, iterations):
+	winnings = []
+	playTimes = []
+	for i in range(iterations):
+		winning, playTime = game()
+		winnings.append(winning)
+		playTimes.append(playTime)
+	printStats(winnings, playTimes)
+
+def printStats(winnings, playTimes):
+	winnings = map(float, winnings)
+	playTimes = map(float, playTimes)
+	# Expected winnings
+	expectedWinnings = 0
+	for winning in winnings:
+		expectedWinnings += winning
+	expectedWinnings /= len(winnings)
+
+	# Proportion of wins
+	wins = 0
+	for winning in winnings:
+		if winning > 0:
+			wins += 1
+	winProportion = wins/len(winnings)
+
+	# Expected playing time
+	playingTime = 0
+	for playTime in playTimes:
+		playingTime += playTime
+	playingTime /= len(playTimes)
+
+	# Maximum loss
+	maxLoss = abs(min(winnings))
+
+	# Maximum win
+	maxWin = max(winnings)
+
+	print("Expected winnings: {}".format(expectedWinnings))
+	print("Proportion of games won: {}".format(winProportion))
+	print("Expected playing time per game: {}".format(playingTime))
+	print("Maximum loss: ${}".format(maxLoss))
+	print("Maximim win: ${}".format(maxWin))
