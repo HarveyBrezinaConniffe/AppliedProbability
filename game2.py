d3a1f77159960df2822ff540725a165312f83801
Harvey Brezina Conniffe <harvey@Harveys-MacBook-Pro.local>
2021-11-30 14:24:00 +0000

    Changed to use helper functions.

--- game2.py
+++ game2.py
@@ -1,29 +1,12 @@
 import random as rand
+import helpers
 
-def simulateOneGame():
+def game2():
 	slot = rand.randint(0, 36)
 	if slot != 0 and slot == 22:
-		return 35
-	return -1
-    
-# Simulate 10,000 games
-winnings = []
-for i in range(10000000):
-	winnings.append(simulateOneGame())
+		return 35, 1
+	return -1, 1
 
-# Expected winnings
-expectedWinnings = 0
-for winning in winnings:
-	expectedWinnings += winning
-expectedWinnings /= len(winnings)
-
-# Proportion of wins
-wins = 0
-for winning in winnings:
-	if winning > 0:
-		wins += 1
-winProportion = wins/len(winnings)
-
-print ("Game 2:")
-print("Expected winnings: {}".format(expectedWinnings))
-print("Proportion of games won: {}".format(winProportion))
+winnings, playTimes = helpers.simulateGame(game2, 10000)
+helpers.printStats(winnings, playTimes)
+helpers.plotAvgAndVariance(winnings)
